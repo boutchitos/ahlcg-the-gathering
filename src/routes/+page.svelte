522eaed882038c13096951a7b792d359cb583dc3
Martin Boucher <boutchitos@yahoo.com>
2023-12-05 21:06:33 -0500

    Prerender all cards listing from arkhamdb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import ahdbCards from '$lib/ahdb.cards.json';
+
+  const uiSubname = (subname: string | undefined) => {
+    return subname ?? false ? ` - (${subname})` : '';
+  };
+
+  const getCards = () => {
+    const toRemove = ['Random Basic Weakness'];
+    return ahdbCards.filter((card) => !toRemove.includes(card.name));
+  };
+
+  const cards = getCards();
+</script>
+
+<h1>Arkham Horror LCG : The Gathering</h1>
+
+<p>You own {cards.length} cards!</p>
+
+<ul>
+  {#each cards as { name, real_name, subname }}
+    <li>
+      {name}
+      {uiSubname(subname)}{name !== real_name ? '=== REAL NAME DIFF. ===============' : ''}
+    </li>
+  {/each}
+</ul>
