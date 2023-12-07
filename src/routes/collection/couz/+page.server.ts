34b74ce9a1fbd153405db48e92790878aee779fa
Martin Boucher <boutchitos@yahoo.com>
2023-12-07 12:56:30 -0500

    Add investigator cards on /collection page

--- src/routes/collection/couz/+page.server.ts
+++ src/routes/collection/couz/+page.server.ts
@@ -1,5 +1,6 @@
+import ahdbCards from '$lib/server/ahdb.cards.json';
 import ahdbPacks from '$lib/server/ahdb.packs.json';
-import packsCollection from '$lib/collections/couz.json';
+import packList from '$lib/collections/couz.json';
 import type { PageServerLoad } from './$types';
 
 type CollectionPack = {
@@ -8,17 +9,58 @@ type CollectionPack = {
   packCode: string;
 };
 
-type Pack = {
+type Card = {
   name: string;
-  code: string;
-  position: number;
-  cycle_position: number;
+  pack_code: string;
+  subname: string;
+  url: string;
+  xp: number;
+};
+
+type Pack = {
   available: string;
+  code: string;
+  cycle_position: number;
+  id: number;
   known: number;
+  name: string;
+  position: number;
   total: number;
   url: string;
-  id: number;
 };
+
+function getCardsByPackCode(ahdbCards: Card[]) {
+  const cardsByPackCode = new Map<string, Card[]>();
+  for (const card of ahdbCards) {
+    if (!cardsByPackCode.has(card.pack_code)) {
+      cardsByPackCode.set(card.pack_code, []);
+    }
+    cardsByPackCode.get(card.pack_code)?.push(card);
+  }
+  return cardsByPackCode;
+}
+
+function getInvestigatorCards(
+  cardsByPackCode: Map<string, Card[]>,
+  packsCollection: (Pack & CollectionPack)[],
+) {
+  const cards = new Array<Card[]>();
+  for (const pack of packsCollection) {
+    const cardsInPack = cardsByPackCode.get(pack.packCode);
+    if (cardsInPack === undefined) {
+      // Carnevale of Horrors n'a pas de cartes investigator.
+      // Je me rends compte que je dois donner 'encounter=1' à l'API pour avoir toutes les cartes.
+      // Ceci dit, j'ai cherché dans les cartes qui devraient seulement être investigator, et je
+      // trouve des '"faction_code": "mythos"'. C'est étrange et à démêler plus tard.
+      if (['coh', 'lol'].includes(pack.packCode)) {
+        continue;
+      }
+      throw new Error(`unknown pack '${pack.packCode}' in collection`);
+    }
+    cards.push(cardsInPack);
+  }
+  return cards.flat();
+}
 
 function getPacksByCode(ahdbPacks: Pack[]) {
   const packsByCode = new Map<string, Pack>();
@@ -32,12 +74,18 @@ function getPacksByCode(ahdbPacks: Pack[]) {
 }
 
 const packsByCode: Map<string, Pack> = getPacksByCode(ahdbPacks);
+const cardsByPackCode: Map<string, Card[]> = getCardsByPackCode(
+  ahdbCards.filter((card) => !['Random Basic Weakness'].includes(card.name)),
+);
+const packsCollection = packList.map((collPack: CollectionPack) =>
+  Object.assign({}, packsByCode.get(collPack.packCode), collPack),
+);
+const investigatorCardsCollection = getInvestigatorCards(cardsByPackCode, packsCollection);
 
 export const load: PageServerLoad = () => {
   return {
     username: 'Couz',
-    packsCollection: packsCollection.map((collPack: CollectionPack) =>
-      Object.assign({}, packsByCode.get(collPack.packCode), collPack),
-    ),
+    packsCollection,
+    investigatorCardsCollection,
   };
 };
--- src/routes/collection/couz/+page.svelte
+++ src/routes/collection/couz/+page.svelte
@@ -2,16 +2,38 @@
   import type { PageData } from './$types';
 
   export let data: PageData;
+
+  type Card = {
+    name: string;
+    pack_code: string;
+    subname: string;
+    url: string;
+    xp: number;
+  };
+
+  const getDisplayName = (card: Card) => {
+    const subname = card.subname ? `: ${card.subname}` : '';
+    const xp = card.xp ? ` (${card.xp} xp)` : '';
+    return `${card.name}${subname}${xp}`;
+  };
 </script>
 
 <h1>{data.username}'s Investigator Cards Collection</h1>
 
 <p>You own {data.packsCollection.length} packs</p>
-
 <ul>
   {#each data.packsCollection as pack}
     <li>
       {pack.nbCopies ?? 1}x {pack.name}
+    </li>
+  {/each}
+</ul>
+
+<p>You own {data.investigatorCardsCollection.length} investigator cards</p>
+<ul>
+  {#each data.investigatorCardsCollection as card}
+    <li>
+      <a href={card.url} target="_blank">{getDisplayName(card)}</a>
     </li>
   {/each}
 </ul>
