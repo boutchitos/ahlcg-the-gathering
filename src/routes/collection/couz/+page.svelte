8683ad3a267d421c766b1e7889e0e59021788052
Martin Boucher <boutchitos@yahoo.com>
2023-12-24 16:57:30 -0500

    Browsing collection pages
    By mouse click on pages.
    Display current page.

--- src/routes/collection/couz/+page.svelte
+++ src/routes/collection/couz/+page.svelte
@@ -4,6 +4,8 @@
   import PocketSheet from './PocketSheet.svelte';
   export let data: PageData;
 
+  let pocketOffset = 0;
+
   const slot = (card: Card) => (card.type_code === 'asset' && card.slot ? ` -- ${card.slot}` : '');
   const title = (card: Card) => {
     const subname = card.subname ? `: ${card.subname}` : '';
@@ -16,17 +18,37 @@
     const packsToInsert = ['ptc', 'eotp', 'tuo', 'apot', 'tpm', 'bsr', 'dca'];
     return packsToInsert.includes(card.pack_code);
   };
+
+  $: currentPage = Math.ceil(pocketOffset / 9) + 1;
+  $: howManyPages = Math.ceil(data.pockets.length / 9);
+
+  function gotoToNextpage() {
+    pocketOffset += 18;
+  }
+
+  function gotoPreviousPage() {
+    pocketOffset -= 18;
+    pocketOffset = pocketOffset < 0 ? 0 : pocketOffset;
+  }
+
 </script>
 
 <h1 class="text-4xl font-bold">{data.username}'s Investigator Cards Collection</h1>
 
+<h2 class="text-2xl font-bold">Showing {currentPage}-{currentPage+1} of {howManyPages} pages</h2>
+
 <!-- un binder, list of PocketSheet/ front-back avec un pager-->
 <div class="mx-auto flex justify-center">
   <div class="grid grid-cols-2 gap-2">
-    <PocketSheet pockets={data.pockets.slice(9, 18)} />
-    <PocketSheet pockets={data.pockets.slice(40, 49)} />
+    <button on:click={gotoPreviousPage}>
+      <PocketSheet pockets={data.pockets.slice(pocketOffset, pocketOffset + 9)} />
+    </button>
+    <button on:click={gotoToNextpage}>
+      <PocketSheet pockets={data.pockets.slice(pocketOffset + 9, pocketOffset + 9 + 9)} />
+    </button>
   </div>
 </div>
+
 <!-- <p>You own {data.investigatorCardsCollection.length} investigator cards</p>
 {#each data.pockets as pocket}
   <ul>
--- src/routes/collection/couz/PocketSheet.svelte
+++ src/routes/collection/couz/PocketSheet.svelte
@@ -2,16 +2,23 @@
   import type { Pocket } from '$lib/BinderStorage';
 
   export let pockets: Pocket[];
+
+  const shouldRotate = (pocket: Pocket) => {
+    return pocket.cards[0].type_code === 'investigator';
+  };
 </script>
 
-<div class="mx-auto flex justify-center">
-  <div class="grid grid-cols-3 gap-2 bg-gray-700 p-2">
+<div class="mx-auto flex">
+  <div class="grid grid-cols-3 grid-rows-3 gap-2 bg-gray-700 p-2">
     {#each pockets as pocket}
-      <img
-        class="h-auto w-[150px] rounded-md border-2 border-gray-500"
-        src={`https://arkhamdb.com${pocket.cards[0].imagesrc}`}
-        alt={pocket.cards[0].name}
-      />
+      <div class="{shouldRotate(pocket) ? '-rotate-90 h-[207px] w-[150px]' : 'h-[207px] w-[150px]'}">
+        <img
+          class="{shouldRotate(pocket) ? 'min-h-[150px] min-w-[207px]' : ''}
+     rounded-md border-2 border-gray-300"
+          src={`https://arkhamdb.com${pocket.cards[0].imagesrc}`}
+          alt={pocket.cards[0].name}
+        />
+      </div>
     {/each}
   </div>
 </div>
