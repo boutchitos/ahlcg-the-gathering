baf97bed81902f2fb067fbfa34eb9035c88de138
Martin Boucher <boutchitos@yahoo.com>
2023-12-24 16:53:45 -0500

    Display current page

--- src/routes/collection/couz/+page.svelte
+++ src/routes/collection/couz/+page.svelte
@@ -19,27 +19,31 @@
     return packsToInsert.includes(card.pack_code);
   };
 
-  function previousPage() {
+  $: currentPage = Math.ceil(pocketOffset / 9) + 1;
+  $: howManyPages = Math.ceil(data.pockets.length / 9);
+
+  function gotoToNextpage() {
+    pocketOffset += 18;
+  }
+
+  function gotoPreviousPage() {
     pocketOffset -= 18;
     pocketOffset = pocketOffset < 0 ? 0 : pocketOffset;
+  }
 
-    console.log(`click ${pocketOffset}`);
-  }
-  function nextPage() {
-    pocketOffset += 18;
-    console.log(`click ${pocketOffset}`);
-  }
 </script>
 
 <h1 class="text-4xl font-bold">{data.username}'s Investigator Cards Collection</h1>
 
+<h2 class="text-2xl font-bold">Showing {currentPage}-{currentPage+1} of {howManyPages} pages</h2>
+
 <!-- un binder, list of PocketSheet/ front-back avec un pager-->
 <div class="mx-auto flex justify-center">
   <div class="grid grid-cols-2 gap-2">
-    <button on:click={previousPage}>
+    <button on:click={gotoPreviousPage}>
       <PocketSheet pockets={data.pockets.slice(pocketOffset, pocketOffset + 9)} />
     </button>
-    <button on:click={nextPage}>
+    <button on:click={gotoToNextpage}>
       <PocketSheet pockets={data.pockets.slice(pocketOffset + 9, pocketOffset + 9 + 9)} />
     </button>
   </div>
