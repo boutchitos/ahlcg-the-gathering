781a6fcfefa4102c828d42d7ad9e84c38c21210b
Martin Boucher <boutchitos@yahoo.com>
2023-12-26 20:11:38 -0500

    Add icons for classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
-<h1>Arkham Horror LCG : The Gathering</h1>
+<h1 class="text-4xl font-bold">Arkham Horror LCG : The Gathering</h1>
 <div>
   <a href="/cards">All investigator cards</a>
   <a href="/packs">All investigator packs</a>
 </div>
 <div>
   <a href="/collection/couz">Couz's Collection</a>
+</div>
+<div>
+  <a href="/icons">Icons showcase</a>
 </div>
---
+++ src/routes/icons/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import Guardian from './guardian.svg.svelte';
+  import Mystic from './mystic.svg.svelte';
+  import Rogue from './rogue.svg.svelte';
+  import Seeker from './seeker.svg.svelte';
+  import Survivor from './survivor.svg.svelte';
+</script>
+
+<div class="flex">
+  <Guardian class="w-12 border" />
+  <Mystic class="w-12 border" />
+  <Rogue class="w-12 border" />
+  <Seeker class="w-12 border" />
+  <Survivor class="w-12 border" />
+</div>
+<div class="flex">
+  <Guardian class="w-24 border" />
+  <Mystic class="w-24 border" />
+  <Rogue class="w-24 border" />
+  <Seeker class="w-24 border" />
+  <Survivor class="w-24 border" />
+</div>
+<div class="flex">
+  <img class="size-12" src="/icon/guardian.webp" alt="guardian icon" />
+  <img class="size-12" src="/icon/mystic.webp" alt="mystic icon" />
+  <img class="size-12" src="/icon/neutral.webp" alt="neutral icon" />
+  <img class="size-12" src="/icon/rogue.webp" alt="rogue icon" />
+  <img class="size-12" src="/icon/seeker.webp" alt="seeker icon" />
+  <img class="size-12" src="/icon/survivor.webp" alt="survivor icon" />
+</div>
+<div class="flex">
+  <img class="size-24" src="/icon/guardian.webp" alt="guardian icon" />
+  <img class="size-24" src="/icon/mystic.webp" alt="mystic icon" />
+  <img class="size-24" src="/icon/neutral.webp" alt="neutral icon" />
+  <img class="size-24" src="/icon/rogue.webp" alt="rogue icon" />
+  <img class="size-24" src="/icon/seeker.webp" alt="seeker icon" />
+  <img class="size-24" src="/icon/survivor.webp" alt="survivor icon" />
+</div>
