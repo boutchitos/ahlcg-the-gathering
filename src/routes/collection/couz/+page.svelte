d1e6fc5fc06a5310ed8ab744b07ab393be08a141
Martin Boucher <boutchitos@yahoo.com>
2023-12-28 17:16:22 -0500

    Add icons for classes
    Artwork by Lemmingitus (https://www.reddit.com/r/arkhamhorrorlcg/comments/wx2l1b/16bit_pixel_fanart_faction_icons/)

--- src/routes/collection/couz/+page.svelte
+++ src/routes/collection/couz/+page.svelte
@@ -1,23 +1,10 @@
 <script lang="ts">
   import type { PageData } from './$types';
-  import type { Card } from '$lib/BinderStorage';
   import PocketSheet from './PocketSheet.svelte';
+
   export let data: PageData;
 
   let pocketOffset = 0;
-
-  const slot = (card: Card) => (card.type_code === 'asset' && card.slot ? ` -- ${card.slot}` : '');
-  const title = (card: Card) => {
-    const subname = card.subname ? `: ${card.subname}` : '';
-    const level = card.xp ? ` (lvl ${card.xp})` : '';
-    return `${card.name}${subname}${level}`;
-  };
-
-  const toInsert = (card: Card) => {
-    // Investigator Expansion is emulated for Path to Carcosa.
-    const packsToInsert = ['ptc', 'eotp', 'tuo', 'apot', 'tpm', 'bsr', 'dca'];
-    return packsToInsert.includes(card.pack_code);
-  };
 
   $: currentPage = Math.ceil(pocketOffset / 9) + 1;
   $: howManyPages = Math.ceil(data.pockets.length / 9);
@@ -30,12 +17,11 @@
     pocketOffset -= 18;
     pocketOffset = pocketOffset < 0 ? 0 : pocketOffset;
   }
-
 </script>
 
 <h1 class="text-4xl font-bold">{data.username}'s Investigator Cards Collection</h1>
 
-<h2 class="text-2xl font-bold">Showing {currentPage}-{currentPage+1} of {howManyPages} pages</h2>
+<h2 class="text-2xl font-bold">Showing {currentPage}-{currentPage + 1} of {howManyPages} pages</h2>
 
 <!-- un binder, list of PocketSheet/ front-back avec un pager-->
 <div class="mx-auto flex justify-center">
@@ -48,23 +34,3 @@
     </button>
   </div>
 </div>
-
-<!-- <p>You own {data.investigatorCardsCollection.length} investigator cards</p>
-{#each data.pockets as pocket}
-  <ul>
-    {#each pocket.cards as card}
-      {#if toInsert(card)}
-        <li style="color: red">
-          {card.quantity}x <a href={card.url} target="_blank">{title(card)}</a>
-          ({card.faction_code} -- {card.type_code}{slot(card)} -- {card.pack_name})
-        </li>
-      {:else}
-        <li>
-          {card.quantity}x <a href={card.url} target="_blank">{title(card)}</a>
-          ({card.faction_code} -- {card.type_code}{slot(card)} -- {card.pack_name})
-        </li>
-      {/if}
-    {/each}
-  </ul>
-  <hr />
-{/each} -->
