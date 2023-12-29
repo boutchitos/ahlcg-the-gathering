eed173632a328d537774d23721c63c9d17c989ee
Martin Boucher <boutchitos@yahoo.com>
2023-12-29 17:22:35 -0500

    Cleaner

--- src/routes/collection/couz/PocketSheet.svelte
+++ src/routes/collection/couz/PocketSheet.svelte
@@ -3,30 +3,32 @@
 
   export let pockets: Pocket[];
 
-  function minWidthHeight(pocket: Pocket): string {
+  function fitToPocket(): string {
+    return 'inline-flex items-center justify-center';
+  }
+
+  function rotateIfInvestigators(pocket: Pocket): string {
+    return shouldRotate(pocket) ? '-rotate-90' : '';
+  }
+
+  function setMinWidthHeight(pocket: Pocket): string {
     return shouldRotate(pocket) ? 'min-h-[150px] min-w-[209px]' : '';
   }
 
-  function rotateInvestigators(pocket: Pocket): string {
-    return shouldRotate(pocket) ? '-rotate-90' : '';
+  function setWidthHeight(): string {
+    return 'h-[209px] w-[150px]';
   }
 
   function shouldRotate(pocket: Pocket): boolean {
     return pocket.cards[0].type_code === 'investigator';
   }
-
-  function widhtHeight(): string {
-    return 'h-[209px] w-[150px]';
-  }
 </script>
 
 <div class="grid grid-cols-3 grid-rows-3 gap-2 bg-gray-700 p-2">
   {#each pockets as pocket}
-    <div
-      class="inline-flex items-center justify-center {widhtHeight()} {rotateInvestigators(pocket)}"
-    >
+    <div class="{fitToPocket()} {setWidthHeight()} {rotateIfInvestigators(pocket)}">
       <img
-        class="rounded-md border-2 border-gray-300 {minWidthHeight(pocket)}"
+        class="rounded-md border-2 border-gray-300 {setMinWidthHeight(pocket)}"
         src={`https://arkhamdb.com${pocket.cards[0].imagesrc}`}
         alt={pocket.cards[0].name}
       />
