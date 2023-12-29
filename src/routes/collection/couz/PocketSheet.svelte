68cb927d1a0beedcac9904b934ba65251a4a3185
Martin Boucher <boutchitos@yahoo.com>
2023-12-29 16:46:13 -0500

    Same width at div layer

--- src/routes/collection/couz/PocketSheet.svelte
+++ src/routes/collection/couz/PocketSheet.svelte
@@ -11,9 +11,9 @@
 <div class="mx-auto flex">
   <div class="grid grid-cols-3 grid-rows-3 gap-2 bg-gray-700 p-2">
     {#each pockets as pocket}
-      <div class={shouldRotate(pocket) ? 'h-[207px] w-[150px] -rotate-90 inline-flex justify-center items-center' : 'h-[207px] w-[150px]'}>
+      <div class="h-[209px] w-[150px] {shouldRotate(pocket) ? '-rotate-90 inline-flex justify-center items-center' : ''}">
         <img
-          class="{shouldRotate(pocket) ? 'min-h-[150px] min-w-[207px]' : ''}
+          class="{shouldRotate(pocket) ? 'min-h-[150px] min-w-[209px]' : ''}
      rounded-md border-2 border-gray-300"
           src={`https://arkhamdb.com${pocket.cards[0].imagesrc}`}
           alt={pocket.cards[0].name}
