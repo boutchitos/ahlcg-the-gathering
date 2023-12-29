95df05d2ff02d86045d7e71442fdf64348a296d0
Martin Boucher <boutchitos@yahoo.com>
2023-12-29 16:52:25 -0500

    Add subfunctions

--- src/routes/collection/couz/PocketSheet.svelte
+++ src/routes/collection/couz/PocketSheet.svelte
@@ -3,7 +3,15 @@
 
   export let pockets: Pocket[];
 
-  const shouldRotate = (pocket: Pocket) => {
+  function minWidthHeight(pocket:Pocket): string {
+    return shouldRotate(pocket) ? 'min-h-[150px] min-w-[209px]' : ''
+  }
+
+  function rotateInvestigators(pocket: Pocket): string {
+    return shouldRotate(pocket) ? '-rotate-90' : '';
+  }
+
+  function shouldRotate(pocket: Pocket): boolean {
     return pocket.cards[0].type_code === 'investigator';
   };
 </script>
@@ -11,9 +19,13 @@
 <div class="mx-auto flex">
   <div class="grid grid-cols-3 grid-rows-3 gap-2 bg-gray-700 p-2">
     {#each pockets as pocket}
-      <div class="h-[209px] w-[150px] {shouldRotate(pocket) ? '-rotate-90 inline-flex justify-center items-center' : ''}">
+      <div
+        class="inline-flex h-[209px] w-[150px] items-center justify-center {rotateInvestigators(
+          pocket,
+        )}"
+      >
         <img
-          class="{shouldRotate(pocket) ? 'min-h-[150px] min-w-[209px]' : ''}
+          class="{minWidthHeight(pocket)}
      rounded-md border-2 border-gray-300"
           src={`https://arkhamdb.com${pocket.cards[0].imagesrc}`}
           alt={pocket.cards[0].name}
