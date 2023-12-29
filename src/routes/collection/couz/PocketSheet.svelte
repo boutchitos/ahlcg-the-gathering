2fef40379f2d1d1b2d6d3b8f90da6fdf741ce113
Martin Boucher <boutchitos@yahoo.com>
2023-12-29 17:24:00 -0500

    Fix badly rotated images layout (#14)

--- src/routes/collection/couz/PocketSheet.svelte
+++ src/routes/collection/couz/PocketSheet.svelte
@@ -3,22 +3,35 @@
 
   export let pockets: Pocket[];
 
-  const shouldRotate = (pocket: Pocket) => {
+  function fitToPocket(): string {
+    return 'inline-flex items-center justify-center';
+  }
+
+  function rotateIfInvestigators(pocket: Pocket): string {
+    return shouldRotate(pocket) ? '-rotate-90' : '';
+  }
+
+  function setMinWidthHeight(pocket: Pocket): string {
+    return shouldRotate(pocket) ? 'min-h-[150px] min-w-[209px]' : '';
+  }
+
+  function setWidthHeight(): string {
+    return 'h-[209px] w-[150px]';
+  }
+
+  function shouldRotate(pocket: Pocket): boolean {
     return pocket.cards[0].type_code === 'investigator';
-  };
+  }
 </script>
 
-<div class="mx-auto flex">
-  <div class="grid grid-cols-3 grid-rows-3 gap-2 bg-gray-700 p-2">
-    {#each pockets as pocket}
-      <div class={shouldRotate(pocket) ? 'h-[207px] w-[150px] -rotate-90' : 'h-[207px] w-[150px]'}>
-        <img
-          class="{shouldRotate(pocket) ? 'min-h-[150px] min-w-[207px]' : ''}
-     rounded-md border-2 border-gray-300"
-          src={`https://arkhamdb.com${pocket.cards[0].imagesrc}`}
-          alt={pocket.cards[0].name}
-        />
-      </div>
-    {/each}
-  </div>
+<div class="grid grid-cols-3 grid-rows-3 gap-2 bg-gray-700 p-2">
+  {#each pockets as pocket}
+    <div class="{fitToPocket()} {setWidthHeight()} {rotateIfInvestigators(pocket)}">
+      <img
+        class="rounded-md border-2 border-gray-300 {setMinWidthHeight(pocket)}"
+        src={`https://arkhamdb.com${pocket.cards[0].imagesrc}`}
+        alt={pocket.cards[0].name}
+      />
+    </div>
+  {/each}
 </div>
