333a47096de83e4ee7bacad27967cf349b14bc05
Martin Boucher <boutchitos@yahoo.com>
2024-01-03 14:14:11 -0500

    Move arithmetic logic into upmost View Layer

--- src/routes/collection/couz/Binder.svelte
+++ src/routes/collection/couz/Binder.svelte
@@ -1,40 +1,21 @@
 <script lang="ts">
-  import type { Pocket } from './pocket';
   import PocketSheet from './PocketSheet.svelte';
+  import type { Binder } from './pocket';
 
-  export let pockets: Pocket[];
-
-  let pocketOffset = 0;
-
-  $: currentPage = Math.ceil(pocketOffset / 9) + 1;
-  $: howManyPages = Math.ceil(pockets.length / 9);
-  $: leftPockets = getPockets(pockets, pocketOffset);
-  $: rightPockets = getPockets(pockets, pocketOffset, 9);
-
-  function gotoToNextPage() {
-    pocketOffset += 18;
-  }
-
-  function gotoPreviousPage() {
-    pocketOffset -= 18;
-    pocketOffset = pocketOffset < 0 ? 0 : pocketOffset;
-  }
-
-  function getPockets(pockets: Pocket[], pocketOffset: number, offset = 0) {
-    const base = pocketOffset + offset;
-    return pockets.slice(base, base + 9);
-  }
+  export let binder: Binder;
 </script>
 
 <div class="block">
   <div class="grid grid-cols-2 gap-2">
-    <button on:click={gotoPreviousPage}>
-      <PocketSheet pockets={leftPockets} />
+    <button on:click={() => binder.handleLeftPageClick()}>
+      <PocketSheet pockets={binder.leftPage.pockets} />
     </button>
-    <button on:click={gotoToNextPage}>
-      <PocketSheet pockets={rightPockets} />
+    <button on:click={() => binder.handleRightPageClick()}>
+      <PocketSheet pockets={binder.rightPage.pockets} />
     </button>
   </div>
 
-  <h3 class="text-xl font-bold">{currentPage}-{currentPage + 1} of {howManyPages} pages</h3>
+  <h3 class="text-xl font-bold">
+    {binder.currentPage}-{binder.currentPage + 1} of {binder.howManyPages} pages
+  </h3>
 </div>
