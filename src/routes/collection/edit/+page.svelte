17ccb3c56da5ef3cc5193e03f95251618b03d60c
Martin Boucher <boutchitos@yahoo.com>
2024-01-22 15:29:00 -0500

    flex layout

--- src/routes/collection/edit/+page.svelte
+++ src/routes/collection/edit/+page.svelte
@@ -3,11 +3,19 @@
   import PacksBundle from './PacksBundle/PacksBundle.svelte';
 
   const { packsStore } = useCollectionEditor();
+  const title = 'Arkham Horror: The Card Game';
 </script>
 
 <h1 class="text-4xl font-bold">Couz's Investigator Cards Collection</h1>
 
-<div class="grid gap-4">
-  <PacksBundle title="Arkham Horror: The Card Game" packs={$packsStore} />
-  <PacksBundle title="Arkham Horror: The Card Game" packs={$packsStore} />
+<div class="mt-4 flex flex-wrap gap-5">
+  <PacksBundle packs={$packsStore} {title} />
+  <PacksBundle packs={$packsStore} {title} />
+  <PacksBundle packs={$packsStore} {title} />
+  <PacksBundle packs={$packsStore} {title} />
+  <PacksBundle packs={$packsStore} {title} />
+  <PacksBundle packs={$packsStore} {title} />
+  <PacksBundle packs={$packsStore} {title} />
+  <PacksBundle packs={$packsStore} {title} />
+  <PacksBundle packs={$packsStore} {title} />
 </div>
