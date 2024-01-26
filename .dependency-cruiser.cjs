285441c31f8610007e38c647a572b93657d9a070
Martin Boucher <boutchitos@yahoo.com>
2024-01-26 10:25:57 -0500

    remove test and index.ts from arch

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -194,10 +194,10 @@ module.exports = {
        - dynamic: a boolean indicating whether to ignore dynamic (true) or static (false) dependencies.
           leave out if you want to exclude neither (recommended!)
     */
-    // exclude : {
-    //   path: '',
-    //   dynamic: true
-    // },
+    exclude: {
+      path: '.*\.test\..*|.*index\.ts',
+      //   dynamic: true
+    },
 
     /* pattern specifying which files to include (regular expression)
        dependency-cruiser will skip everything not matching this pattern
