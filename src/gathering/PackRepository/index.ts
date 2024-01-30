8e7e9344d9053244c9ba7d45b9184b9e98f52aeb
Martin Boucher <boutchitos@yahoo.com>
2024-01-30 13:49:52 -0500

    fix emulated packs

--- src/gathering/PackRepository/index.ts
+++ src/gathering/PackRepository/index.ts
@@ -8,7 +8,20 @@ export function createPackRepository(): IPackRepository {
 
 class PackRepository implements IPackRepository {
   getAllPacks(): Iterable<Pack> {
-    return ahdbPacks.map((pack) => pack.name);
+    const ahdb = ahdbPacks.map((pack) => pack.name);
+    // those packs are also supported
+    const emulated = [
+      'The Circle Undone Campaign Expansion',
+      'The Circle Undone Investigator Expansion',
+      'The Dunwich Legacy Campaign Expansion',
+      'The Dunwich Legacy Investigator Expansion',
+      'The Forgotten Age Campaign Expansion',
+      'The Forgotten Age Investigator Expansion',
+      'The Path to Carcosa Campaign Expansion',
+      'The Path to Carcosa Investigator Expansion',
+    ];
+
+    return [...ahdb, ...emulated];
   }
 }
 
