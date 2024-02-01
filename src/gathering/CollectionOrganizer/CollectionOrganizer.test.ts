ec52cb8795814a61190f71678392952e19104c51
Martin Boucher <boutchitos@yahoo.com>
2024-02-01 10:12:42 -0500

    test with mocks and vitest mock extended

--- src/gathering/CollectionOrganizer/CollectionOrganizer.test.ts
+++ src/gathering/CollectionOrganizer/CollectionOrganizer.test.ts
@@ -1,33 +1,32 @@
 import { beforeEach, expect, it } from 'vitest';
+import { captor, CaptorMatcher, mock, type MockProxy } from 'vitest-mock-extended';
 import { CollectionEntity } from '$gathering/CollectionEntity';
 import type { Binder, IBinderOutput } from '$gathering/IBinderOutput';
 import { CollectionOrganizer } from './CollectionOrganizer';
 import { PackRepositoryMock } from './PackRepositoryMock';
 
-class BinderOutput implements IBinderOutput {
-  binder: Binder = { pockets: [] };
-
-  binderUpdated(binder: Binder): void {
-    this.binder = binder;
-  }
-}
-
-let binderOutput: BinderOutput;
+let binder: CaptorMatcher<Binder>;
+let binderOutput: MockProxy<IBinderOutput>;
 
 beforeEach(() => {
-  binderOutput = new BinderOutput();
+  binderOutput = mock();
+  binder = captor();
 });
 
 it('organizes an empty collection', () => {
   createOrganizer(createCollection());
 
-  expect(binderOutput.binder.pockets).toStrictEqual([]);
+  expect(binderOutput.binderUpdated).toHaveBeenCalledWith(binder);
+  expect(binder.value).toEqual({ pockets: [] });
 });
 
 it('organizes a collection with 1x Core Set', () => {
   createOrganizer(createCollection('Core Set'));
 
-  const cardsOf1stPocket = binderOutput.binder.pockets[0].cards;
+  expect(binderOutput.binderUpdated).toHaveBeenCalledWith(binder);
+
+  const pockets = binder.value.pockets;
+  const cardsOf1stPocket = pockets[0].cards;
   const roland = cardsOf1stPocket[0];
   const copies = cardsOf1stPocket.filter((card) => card.name === 'Roland Banks');
   expect(roland.name).toStrictEqual('Roland Banks');
@@ -37,33 +36,27 @@ it('organizes a collection with 1x Core Set', () => {
 it('organizes a collection with 2x Core Set', () => {
   createOrganizer(createCollection('Core Set', 'Core Set'));
 
-  const cardsOf1stPocket = binderOutput.binder.pockets[0].cards;
-  const roland = cardsOf1stPocket[0];
+  expect(binderOutput.binderUpdated).toHaveBeenCalledWith(binder);
+
+  const pockets = binder.value.pockets;
+  const cardsOf1stPocket = pockets[0].cards;
   const copies = cardsOf1stPocket.filter((card) => card.name === 'Roland Banks');
-  expect(roland.name).toStrictEqual('Roland Banks');
   expect(copies).toHaveLength(2);
 });
 
 it('updates many outputs', () => {
-  const orgaznier = createOrganizer(createCollection('Core Set'));
+  const organizer = createOrganizer(createCollection('Core Set'));
+  const output2 = mock<IBinderOutput>();
+  organizer.onBinderUpdated(output2);
 
-  const binderOutput2 = new BinderOutput();
-  orgaznier.onBinderUpdated(binderOutput2);
+  expect(output2.binderUpdated).toHaveBeenCalledWith(binder);
 
-  {
-    const cardsOf1stPocket = binderOutput.binder.pockets[0].cards;
-    const roland = cardsOf1stPocket[0];
-    const copies = cardsOf1stPocket.filter((card) => card.name === 'Roland Banks');
-    expect(roland.name).toStrictEqual('Roland Banks');
-    expect(copies).toHaveLength(1);
-  }
-  {
-    const cardsOf1stPocket = binderOutput2.binder.pockets[0].cards;
-    const roland = cardsOf1stPocket[0];
-    const copies = cardsOf1stPocket.filter((card) => card.name === 'Roland Banks');
-    expect(roland.name).toStrictEqual('Roland Banks');
-    expect(copies).toHaveLength(1);
-  }
+  const pockets = binder.value.pockets;
+  const cardsOf1stPocket = pockets[0].cards;
+  const roland = cardsOf1stPocket[0];
+  const copies = cardsOf1stPocket.filter((card) => card.name === 'Roland Banks');
+  expect(roland.name).toStrictEqual('Roland Banks');
+  expect(copies).toHaveLength(1);
 });
 
 function createOrganizer(collection: CollectionEntity) {
