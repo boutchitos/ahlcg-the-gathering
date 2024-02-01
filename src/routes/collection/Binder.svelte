75466f66be9e0094149e07a2d8533c9a18648cef
Martin Boucher <boutchitos@yahoo.com>
2024-02-01 15:12:31 -0500

    update

--- src/routes/collection/Binder.svelte
+++ src/routes/collection/Binder.svelte
@@ -1,27 +1,22 @@
 <script lang="ts">
-  import type {
-    BinderAs2Pages,
-    PocketViewModel,
-  } from '$lib/userBrowsesItsCollection/userBrowsesItsCollection';
+  import type { BinderAs2Pages } from '$lib/userBrowsesItsCollection/userBrowsesItsCollection';
   import PocketSheet from './PocketSheet.svelte';
 
   export let binder: BinderAs2Pages;
 
   let currentPage = binder.currentPage;
   let howManyPages = binder.howManyPages;
-  let leftPockets: PocketViewModel[];
-  let rightPockets: PocketViewModel[];
-  binder.leftPage.subscribe((page) => (leftPockets = page.pockets));
-  binder.rightPage.subscribe((page) => (rightPockets = page.pockets));
+  let leftPage = binder.leftPage;
+  let rightPage = binder.rightPage;
 </script>
 
 <div class="block">
   <div class="grid grid-cols-2 gap-2">
     <button on:click={() => binder.handleLeftPageClick()}>
-      <PocketSheet pockets={leftPockets} />
+      <PocketSheet pockets={$leftPage.pockets} />
     </button>
     <button on:click={() => binder.handleRightPageClick()}>
-      <PocketSheet pockets={rightPockets} />
+      <PocketSheet pockets={$rightPage.pockets} />
     </button>
   </div>
 
