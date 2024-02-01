2a4a442efc98f8fde75daca3843dc70948854b25
Martin Boucher <boutchitos@yahoo.com>
2024-02-01 16:24:59 -0500

    local storage

--- src/routes/collection/+page.svelte
+++ src/routes/collection/+page.svelte
@@ -3,7 +3,12 @@
   import Binder from './Binder.svelte';
   import SortableClasses from '$lib/SortableClasses/SortableClasses.svelte';
 
+  const isBrowser = typeof window !== 'undefined';
+
   const { binder, classes } = userBrowsesItsCollection();
+
+  isBrowser && localStorage.classes && classes.set(JSON.parse(localStorage.classes));
+  $: isBrowser && (localStorage.classes = JSON.stringify($classes));
 </script>
 
 <h1 class="text-4xl font-bold">Couz's Investigator Cards Collection</h1>
