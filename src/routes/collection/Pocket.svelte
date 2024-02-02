2dca946fdb855d767d512f9b76c75a3a6a0ba8b2
Martin Boucher <boutchitos@yahoo.com>
2024-02-02 10:30:45 -0500

    subcomponent Pocket

---
+++ src/routes/collection/Pocket.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { PocketViewModel } from '$lib/userBrowsesItsCollection/userBrowsesItsCollection';
+
+  export let pocket: PocketViewModel;
+
+  function rotateLandscape(pocket: PocketViewModel): string {
+    return pocket.coverImage.landscape ? '-rotate-90' : '';
+  }
+
+  function setMinWidthHeight(pocket: PocketViewModel): string {
+    return pocket.coverImage.landscape ? 'min-h-[150px] min-w-[209px]' : '';
+  }
+</script>
+
+<div class="inline-flex h-[209px] w-[150px] items-center justify-center">
+  <img
+    class="rounded-md border-2 border-gray-300 {setMinWidthHeight(pocket)} {rotateLandscape(
+      pocket,
+    )}"
+    src={pocket.coverImage.url}
+    alt={pocket.title}
+  />
+</div>
--- src/routes/collection/PocketSheet.svelte
+++ src/routes/collection/PocketSheet.svelte
@@ -1,27 +1,12 @@
 <script lang="ts">
   import type { PocketViewModel } from '$lib/userBrowsesItsCollection/userBrowsesItsCollection';
+  import Pocket from './Pocket.svelte';
 
   export let pockets: PocketViewModel[];
-
-  function rotateLandscape(pocket: PocketViewModel): string {
-    return pocket.coverImage.landscape ? '-rotate-90' : '';
-  }
-
-  function setMinWidthHeight(pocket: PocketViewModel): string {
-    return pocket.coverImage.landscape ? 'min-h-[150px] min-w-[209px]' : '';
-  }
 </script>
 
 <div class="grid grid-cols-3 grid-rows-3 gap-2 bg-gray-700 p-2">
   {#each pockets as pocket}
-    <div class="inline-flex h-[209px] w-[150px] items-center justify-center">
-      <img
-        class="rounded-md border-2 border-gray-300 {setMinWidthHeight(pocket)} {rotateLandscape(
-          pocket,
-        )}"
-        src={pocket.coverImage.url}
-        alt={pocket.title}
-      />
-    </div>
+    <Pocket {pocket} />
   {/each}
 </div>
