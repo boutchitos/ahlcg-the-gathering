db783481e044f850cdf3794b24ada1b2f3bc993f
Martin Boucher <boutchitos@yahoo.com>
2024-02-19 08:43:12 -0500

    add /collection/organize page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,12 +17,13 @@
 <div class="flex h-screen flex-col items-center">
   <div class="flex gap-4 text-lg">
     <a href="/collection/">View your Collection</a>
+    <a href="/collection/organize">Organize your Collection</a>
     <a href="/collection/edit">Edit your Collection</a>
   </div>
 
   <slot />
 
-  <h1 class="sticky top-[100vh] justify-center text-4xl font-bold">
+  <h1 class="sticky top-[100vh] mb-3 justify-center text-4xl">
     The Gathering, a binder storage solution for Arkham Horror: The Card Game
   </h1>
 </div>
