64affa35b5ef79bfe3bd211ac5950cb492f8a7cf
Martin Boucher <boutchitos@yahoo.com>
2024-02-19 10:16:44 -0500

    add checkboxes

--- src/routes/collection/organize/+page.svelte
+++ src/routes/collection/organize/+page.svelte
@@ -4,11 +4,15 @@
   import SortablePlayerCardTypes from '$lib/SortablePlayerCardTypes/SortablePlayerCardTypes.svelte';
   import SortableSlots from '$lib/SortableSlots/SortableSlots.svelte';
   import { userBrowsesItsCollection } from '$lib/userBrowsesItsCollection/userBrowsesItsCollection';
+  import { writable } from 'svelte/store';
   import { SortPlayerCardsDirectivesConfig } from '../SortPlayerCardsDirectivesConfig';
 
   const { classes, playerCardTypes, slots, sortingOrder } = userBrowsesItsCollection(
     new SortPlayerCardsDirectivesConfig(),
   );
+
+  let groupIfSameTitle = writable(false);
+  let groupOfAnyLevels = writable(false);
 </script>
 
 <SortableCriterias items={sortingOrder} let:item>
@@ -23,3 +27,13 @@
   {/if}
 </SortableCriterias>
 <SortableSlots {slots} />
+
+<label>
+  <input type="checkbox" bind:checked={$groupIfSameTitle} />
+  Group cards with same title
+</label>
+
+<label>
+  <input type="checkbox" disabled={!$groupIfSameTitle} bind:checked={$groupOfAnyLevels} />
+  Group cards of any levels
+</label>
