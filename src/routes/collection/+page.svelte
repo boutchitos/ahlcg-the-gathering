142fcc02a0f3638a101023ba3d377bd480b85af3
Martin Boucher <boutchitos@yahoo.com>
2024-10-10 13:12:03 -0400

    start new layout with sidebar

--- src/routes/collection/+page.svelte
+++ src/routes/collection/+page.svelte
@@ -6,6 +6,6 @@
   const { binder } = userBrowsesItsCollection(new SortPlayerCardsDirectivesConfig());
 </script>
 
-<h1 class="mb-4 text-4xl font-bold">Couz's Investigator Cards Collection</h1>
+<h1 class="mb-4 text-center text-4xl font-bold">Couz's Investigator Cards Collection</h1>
 
 <Binder {binder} />
