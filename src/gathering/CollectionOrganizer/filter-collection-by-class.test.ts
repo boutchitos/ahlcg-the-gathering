620f683a83750279652a51c2e422f94fb2880ebd
Martin Boucher <boutchitos@yahoo.com>
2024-10-15 13:11:15 -0400

    wip

--- src/gathering/CollectionOrganizer/filter-collection-by-class.test.ts
+++ src/gathering/CollectionOrganizer/filter-collection-by-class.test.ts
@@ -7,13 +7,14 @@ import { findPocketWithCard } from './test-utils/pockets';
 import { setupOrganizer } from './test-utils/setupOrganizer';
 
 it('filters a collection with a class', () => {
+  const binder = captor<Binder>();
   const { binderOutput, organizer } = setupOrganizer('Core Set');
+  expect(binderOutput.binderUpdated).toHaveBeenCalledWith(binder);
+  expect(findPocketWithCard(binder.value.pockets, 'Agnes Baker')).toBeDefined();
   mockClear(binderOutput);
 
   organizer.filterByClass('guardian');
+  expect(binderOutput.binderUpdated).toHaveBeenCalledWith(binder);
 
-  const guardianBinder = captor<Binder>();
-  expect(binderOutput.binderUpdated).toHaveBeenCalledWith(guardianBinder);
-
-  expect(findPocketWithCard(guardianBinder.value.pockets, 'Agnes Baker')).toBeUndefined();
+  expect(findPocketWithCard(binder.value.pockets, 'Agnes Baker')).toBeUndefined();
 });
