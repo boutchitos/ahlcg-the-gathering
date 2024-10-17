3b26c85ac9bf0258499ac9ae372329d353475b4a
Martin Boucher <boutchitos@yahoo.com>
2024-10-17 08:45:15 -0400

    wip

--- src/gathering/CollectionOrganizer/classify-player-cards/classifyPlayerCards.ts
+++ src/gathering/CollectionOrganizer/classify-player-cards/classifyPlayerCards.ts
@@ -15,12 +15,11 @@ export function classifyPlayerCards(cards: Iterable<Card>) {
     'basic weakness': [],
   };
 
-  const cardsByCode = new Map<string, Card>([...cards].map((card) => [card.code, card]));
-  const cardsByName = new Map<string, Card>([...cards].map((card) => [card.name, card]));
+  const cardsClassifier = new CardClassifier(cards);
 
   for (const card of cards) {
     if (card.restrictions?.investigator !== undefined) {
-      const investigatorClass = getRestrictedToInvestigatorClass(card, cardsByCode);
+      const investigatorClass = cardsClassifier.getRestrictedToInvestigatorClass(card);
       classified[investigatorClass].push(card);
     } else if (card.name === 'Aetheric Current') {
       // bonded to a restricted card (Flux Stabilizer) for Kate Winthrop
@@ -31,13 +30,8 @@ export function classifyPlayerCards(cards: Iterable<Card>) {
       // we could chain that
       classified.mystic.push(card);
     } else if (card.bonded_to !== undefined) {
-      const bondedToCard = cardsByName.get(card.bonded_to);
-      if (bondedToCard === undefined) {
-        throw Error(
-          `bonded to card not found for bonded card ${JSON.stringify(card, undefined, 2)}`,
-        );
-      }
-      classified[bondedToCard.playerCardClass].push(card);
+      const bondedToCardClass = cardsClassifier.getBondedToCardClass(card);
+      classified[bondedToCardClass].push(card);
     } else {
       classified[card.playerCardClass].push(card);
     }
@@ -45,31 +39,48 @@ export function classifyPlayerCards(cards: Iterable<Card>) {
   return classified;
 }
 
-// On fouille juste dans les cartes de la collection. On en veut un. Si plusieurs, c'est le même.
-function getRestrictedToInvestigatorClass(
-  card: Card,
-  cardsByCode: Map<string, Card>,
-): PlayerCardClass {
-  const restrictedToInvestigatorClasses = new Set<PlayerCardClass>();
+class CardClassifier {
+  private readonly cardsByCode: Map<string, Card>;
+  private readonly cardsByName: Map<string, Card>;
 
-  for (const [kCode, vCode] of Object.entries(card.restrictions!.investigator)) {
-    if (kCode !== vCode) {
+  constructor(cards: Iterable<Card>) {
+    this.cardsByCode = new Map([...cards].map((card) => [card.code, card]));
+    this.cardsByName = new Map([...cards].map((card) => [card.name, card]));
+  }
+
+  public getBondedToCardClass(card: Card): PlayerCardClass {
+    if (card.bonded_to === undefined) {
+      throw Error(`shoul have bonded card to call... will refact soon`);
+    }
+    const bondedToCard = this.cardsByName.get(card.bonded_to);
+    if (bondedToCard === undefined) {
+      throw Error(`bonded to card not found for bonded card ${JSON.stringify(card, undefined, 2)}`);
+    }
+    return bondedToCard.playerCardClass;
+  }
+
+  public getRestrictedToInvestigatorClass(card: Card): PlayerCardClass {
+    const restrictedToInvestigatorClasses = new Set<PlayerCardClass>();
+
+    for (const [kCode, vCode] of Object.entries(card.restrictions!.investigator)) {
+      if (kCode !== vCode) {
+        throw Error(
+          `Bad assomption: keys and values are always the same ${JSON.stringify(card, undefined, 2)}`,
+        );
+      }
+
+      const investigator = this.cardsByCode.get(kCode);
+      if (investigator !== undefined) {
+        restrictedToInvestigatorClasses.add(investigator.playerCardClass);
+      }
+    }
+
+    if (restrictedToInvestigatorClasses.size !== 1) {
       throw Error(
-        `Bad assomption: keys and values are always the same ${JSON.stringify(card, undefined, 2)}`,
+        `After further analysis, this card sucks to find its class ${JSON.stringify(card, undefined, 2)}`,
       );
     }
 
-    const investigator = cardsByCode.get(kCode);
-    if (investigator !== undefined) {
-      restrictedToInvestigatorClasses.add(investigator.playerCardClass);
-    }
+    return [...restrictedToInvestigatorClasses][0];
   }
-
-  if (restrictedToInvestigatorClasses.size !== 1) {
-    throw Error(
-      `After further analysis, this card sucks to find its class ${JSON.stringify(card, undefined, 2)}`,
-    );
-  }
-
-  return [...restrictedToInvestigatorClasses][0];
 }
